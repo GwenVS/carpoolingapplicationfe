ee72c9e8b9025b8bbcf5d4b9b4b5b3e5d86b1e62
gwendolenevansteenkiste <gwendolene.vansteenkiste@student.kdg.be>
2018-07-14 18:13:19 +0200

    ...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/carpoolingapplicationfe'));
+app.use(express.static(__dirname + '/dist'));
 
 /*app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/carpoolingapplicationfe/index.html'));
