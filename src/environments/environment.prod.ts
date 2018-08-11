07f9fbcc4fd36b9fe4b69adfadb1d122d2788636
gwendolenevansteenkiste <gwendolene.vansteenkiste@student.kdg.be>
2018-08-11 20:36:42 +0200

    tryout deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true
 };
 
-// export const serverUrl = 'https://carpoolingapplication.herokuapp.com';
-export const serverUrl = 'http://localhost:9090';
+export const serverUrl = 'https://carpoolingapplication.herokuapp.com';
+//export const serverUrl = 'http://localhost:9090';
 
-//export const frontendUrl = 'https://carpoolingapplicationfe.herokuapp.com';
-export const frontendUrl = 'http://localhost:4200';
+export const frontendUrl = 'https://carpoolingapplicationfe.herokuapp.com';
+//export const frontendUrl = 'http://localhost:4200';
