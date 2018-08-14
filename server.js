bd60be1d72d0d399c1425b9ecfc8bb012924ece7
gwendolenevansteenkiste <gwendolene.vansteenkiste@student.kdg.be>
2018-08-15 00:03:35 +0200

    try

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+/*
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});*/
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 9090);
